d8e18571c213e7377c06a40e04f1583bf5c1f876
X0lotl <danil200319771@gmail.com>
2022-07-14 11:20:51 +0300

    first commit

---
+++ src/WasjaWolkSocialCreditTelegBot.java
@@ -0,0 +1,8 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class WasjaWolkSocialCreditTelegBot extends TelegramLongPollingBot{
+  @Override
+  public String getBotToken() {
+    return null;
+  }
+}
