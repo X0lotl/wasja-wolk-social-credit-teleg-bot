55928fc2bf9ab333faf101c84ad6296ae9bf2733
X0lotl <danil200319771@gmail.com>
2022-07-14 16:41:53 +0300

    add three common commands

--- src/WasjaWolkSocialCreditTelegBot.java
+++ src/WasjaWolkSocialCreditTelegBot.java
@@ -4,7 +4,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WasjaWolkSocialCreditTelegBot extends TelegramLongPollingBot {
   @Override
@@ -22,48 +27,62 @@ public class WasjaWolkSocialCreditTelegBot extends TelegramLongPollingBot {
   @Override
   public void onUpdateReceived(Update update) {
     if (update.hasMessage() && update.getMessage().hasText()) {
-
-      WasjaWolk wasjaWolk = new WasjaWolk();
-
       Message message = update.getMessage();
       String message_text = message.getText();
 
-      String userName = message.getChat().getUserName();
-      long id = message.getChat().getId();
-      long chat_id = update.getMessage().getChatId();
+      WasjaWolk wasjaWolk = new WasjaWolk();
+      boolean isWasjaWolk = isWasjaWolk(message, wasjaWolk);
+      boolean hasAnswer = false;
 
+      long chat_id = update.getMessage().getChatId();
 
       SendMessage sendMessage = new SendMessage();
       sendMessage.setChatId(chat_id);
 
+      ReplyKeyboardMarkup keyboard = generateKeyBoard();
+      sendMessage.setReplyMarkup(keyboard);
+
       System.out.println(update.getMessage());
 
-      if (userName.equals(wasjaWolk.getUserName()) || id == wasjaWolk.getId()) {
+      System.out.println(message_text);
+
+      if (message_text.equals("/start")) {
+        hasAnswer = true;
+        sendMessage.setText("Hello everybody! We change @"
+          + wasjaWolk.getUserName() +
+          "'s social credits so often, it's easy to wonder how many extra bowls of rice he has. " +
+          "Therefore, I was created so that you can precisely follow this indicator and change it :).");
+      }
+      if (message_text.equals("plus") && !isWasjaWolk) {
+        hasAnswer = true;
+        sendMessage.setText("@" + wasjaWolk.getUserName() + " My congratulations, your social rating has been increased :)\n" +
+          "Now it is:\n" +
+          wasjaWolk.get_socialCredits());
+      }
+      if (message_text.equals("plus") && isWasjaWolk){
+        hasAnswer = true;
         sendMessage.setText("Danya, don't think that you can change your social rating");
-      } else {
-        if (message_text.equals("/start")) {
-          sendMessage.setText("Hello everybody! We change @"
-            + wasjaWolk.getUserName() +
-            "'s social credits so often, it's easy to wonder how many extra bowls of rice he has. " +
-            "Therefore, I was created so that you can precisely follow this indicator and change it :).");
-        }
-        if (message_text.equals("/plus")) {
-          sendMessage.setText("@" + wasjaWolk.getUserName() + " My congratulations, your social rating has been increased :)\n" +
-            "Now it is:\n" +
-            wasjaWolk.get_socialCredits());
-        }
       }
 
-      if (message_text.equals("/minus")) {
+      if (message_text.equals("minus") && !isWasjaWolk) {
+        hasAnswer = true;
         sendMessage.setText("@" + wasjaWolk.getUserName() + " It's a shame, your social credits has been lowered :(\n" +
           "Now it is:\n" +
           wasjaWolk.get_socialCredits());
       }
+      if(message_text.equals("minus") && isWasjaWolk){
+        hasAnswer = true;
+        sendMessage.setText("Danya, don't think that you can change your social rating");
+      }
 
       if (message_text.equals("/credits")) {
+        hasAnswer = true;
         sendMessage.setText("@" + wasjaWolk.getUserName() + "\n" +
           "Your social credits\n" +
           wasjaWolk.get_socialCredits());
+      }
+      if(!hasAnswer){
+        sendMessage.setText("Unknown command");
       }
 
 
@@ -73,5 +92,25 @@ public class WasjaWolkSocialCreditTelegBot extends TelegramLongPollingBot {
         e.printStackTrace();
       }
     }
+  }
+
+  private ReplyKeyboardMarkup generateKeyBoard() {
+
+    ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+    List<KeyboardRow> keyboard = new ArrayList<>();
+    KeyboardRow row = new KeyboardRow();
+
+    row.add("plus");
+    row.add("minus");
+    row.add("social credits");
+
+    keyboard.add(row);
+    keyboardMarkup.setKeyboard(keyboard);
+
+    return keyboardMarkup;
+  }
+
+  private boolean isWasjaWolk(Message message, WasjaWolk wasjaWolk) {
+    return message.getChat().getUserName().equals(wasjaWolk.getUserName()) || message.getChat().getId() == wasjaWolk.getId();
   }
 }
