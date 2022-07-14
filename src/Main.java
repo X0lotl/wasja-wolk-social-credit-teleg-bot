4742464b509b6f3a846bd535001de4a6643c7203
X0lotl <danil200319771@gmail.com>
2022-07-14 11:50:43 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Main {
+  public static void main(String[] args) {
+  }
+}
--- src/WasjaWolkSocialCreditTelegBot.java
+++ src/WasjaWolkSocialCreditTelegBot.java
@@ -1,8 +1,21 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class WasjaWolkSocialCreditTelegBot extends TelegramLongPollingBot{
+public class WasjaWolkSocialCreditTelegBot extends TelegramLongPollingBot {
+  @Override
+  public String getBotUsername() {
+    return null;
+  }
+
   @Override
   public String getBotToken() {
-    return null;
+    Dotenv dotenv = Dotenv.configure().load();
+    return dotenv.get("BOT_TOKKEN");
+  }
+
+  @Override
+  public void onUpdateReceived(Update update) {
+
   }
 }
